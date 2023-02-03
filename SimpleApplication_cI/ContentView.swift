382b2de17b4b251ad6057a68cc98e2eb6f97a978
Muhammad Rehan <mrehanmeo@gmail.com>
2023-02-04 04:41:26 +0500

    Initial Commit

---
+++ SimpleApplication_cI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleApplication_cI
+//
+//  Created by Muhammad Rehan on 2/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleApplication_cI/SimpleApplication_cIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleApplication_cIApp.swift
+//  SimpleApplication_cI
+//
+//  Created by Muhammad Rehan on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleApplication_cIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
